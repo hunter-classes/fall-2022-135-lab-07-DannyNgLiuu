19a8a471fedc403a7be28f819f4362de312942e1
Danny Ng <danny@Dannys-MacBook-Pro.local>
2022-12-08 09:35:13 -0500

    added comment

--- unindent.cpp
+++ unindent.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "removeLeadingSpaces.h"
-
+//gets the input and removes all leading spaces using the removeLeadingSpaces function
 int main() {
     std::string response;
     std::string oneLine;
