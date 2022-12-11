70905aa3c2b4f5194f5307e2fc2cb9da3a006d47
DannyNgLiuu <112090611+DannyNgLiuu@users.noreply.github.com>
2022-12-11 15:51:31 -0500

    Revert "Patrick's Pull Request to Danny"

--- countChar.cpp
+++ countChar.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 #include <string>
 
-/*
-    CountChar(string, char)
-
-    counts the frequency of a char
-
-*/
 int countChar(std::string line, char c) {
     int counter = 0;
     for(int i = 0; i < line.length(); i++) {
